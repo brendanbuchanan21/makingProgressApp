bf213febf54e6867af4605c2d67a8401de8f71ac
Brendan Buchanan <brendanbuchanan21@gmail.com>
2025-04-05 13:47:26 -0500

    removed window alert and added pop up conditionals for when someone attempts to delete a planned week in a program

--- frontend/src/components/workouts/currentPlan/weekCard.tsx
+++ frontend/src/components/workouts/currentPlan/weekCard.tsx
@@ -22,128 +22,138 @@ interface WeekCardProps {
 const WeekCard: React.FC<WeekCardProps> = ({ isEditing }) => {
 
     
+  //redux state
+  const currentPlan = useSelector((state: RootState) => state.workout.currentPlan);
+  const weeks = currentPlan?.weeks ?? [];
+  const workoutPlanId = currentPlan?._id;
 
-    const currentPlan = useSelector((state: RootState) => state.workout.currentPlan);
-    const weeks = currentPlan?.weeks ?? [];
-    const workoutPlanId = currentPlan?._id;
-    const [isUserReady, setIsUserReady] = useState(false);
-    const [userId, setUserId] = useState<string | null>(null)
+
+  //use state values
+  const [isUserReady, setIsUserReady] = useState(false);
+  const [userId, setUserId] = useState<string | null>(null)
+  const [warningMessage, setWarningMessage] = useState(false);
+  const [deleteWeekPopUp, setDeleteWeekPopUp] = useState(false);
+  const [weekToDelete, setWeekToDelete] = useState<number | null>(null);
    
+  //imported necessities
+  const dispatch = useDispatch();
+  const auth = getAuth();
 
-    const dispatch = useDispatch();
-    
 
-     const auth = getAuth();
-        useEffect(() => {
-            const unsubscribe = onAuthStateChanged(auth, (user) => {
-                if(user) {
-                    setUserId(user.uid);
-                    setIsUserReady(true)
-                } else {
-                    setUserId(null);
-                    setIsUserReady(false);
-                }
-            });
-            //clean up on unmount
-            return () => unsubscribe();
-        }, [auth]);
+useEffect(() => {
+  const unsubscribe = onAuthStateChanged(auth, (user) => {
+  if(user) {
+  setUserId(user.uid);
+  setIsUserReady(true)
+  } else {
+  setUserId(null);
+  setIsUserReady(false);
+  }
+  });
+  //clean up on unmount
+  return () => unsubscribe();
+}, [auth]);
 
   // Ensure the query is only sent when the token is ready
   const { data: workoutPlanData, error: workoutPlanError, isLoading: isLoadingWorkoutPlan } = useGetExerciseProgramQuery(
-    { workoutPlanId: workoutPlanId },
-    {
-      skip: !isUserReady,
-      refetchOnMountOrArgChange: true,
-    }
-    
-  );
+  { workoutPlanId: workoutPlanId },
+  {
+  skip: !isUserReady,
+  refetchOnMountOrArgChange: true,
+  });
     
     
     
   // Second query: Get completed workout volume
   const { data: completedWorkoutData, error: completedWorkoutsError, isLoading: isLoadingCompletedWorkouts } = useGetCompletedWorkoutVolumeQuery(
-    workoutPlanId,
-    {
-      skip: !isUserReady, // Ensure it doesn't run until the user is ready
-    refetchOnMountOrArgChange: true, // Ensure it refetches if workoutPlanId changes
-    }
+  workoutPlanId,
+  {
+  skip: !isUserReady, // Ensure it doesn't run until the user is ready
+  refetchOnMountOrArgChange: true, // Ensure it refetches if workoutPlanId changes
+  }
   );
 
    
 
-    useEffect(() => {
-      if(workoutPlanData && !currentPlan) {
-        dispatch(setCurrentPlan(workoutPlanData));
-      }
-    }, [workoutPlanData, dispatch, currentPlan])
+useEffect(() => {
+  if(workoutPlanData && !currentPlan) {
+  dispatch(setCurrentPlan(workoutPlanData));
+  }
+}, [workoutPlanData, dispatch, currentPlan])
     // Fetch completed workouts for the plan
 
     
-    const [deleteWeekApi] = useDeleteWeekApiMutation();
+  const [deleteWeekApi] = useDeleteWeekApiMutation();
     
-    // Group the completed workouts by weekNumber and sum totalVolume for each week
-    let volumeByWeek: { [key: number]: number } = {};
+  // Group the completed workouts by weekNumber and sum totalVolume for each week
+  let volumeByWeek: { [key: number]: number } = {};
 
   // Group the completed workouts by weekNumber and sum totalVolume for each week
   if (Array.isArray(completedWorkoutData)) {
-    volumeByWeek = completedWorkoutData.reduce((acc: { [key: number]: number }, workout: any) => {
-      const weekNum = workout.weekNumber;
+  volumeByWeek = completedWorkoutData.reduce((acc: { [key: number]: number }, workout: any) => {
+  const weekNum = workout.weekNumber;
   
-      // Only initialize week number if it doesn't already have a value
-      if (acc[weekNum] === undefined) {
-        acc[weekNum] = 0;
-      }
+  // Only initialize week number if it doesn't already have a value
+  if (acc[weekNum] === undefined) {
+  acc[weekNum] = 0;
+  }
   
-      // Add the workout volume to the week
-      acc[weekNum] += workout.totalVolume;
-      return acc;
-    }, {});
+  // Add the workout volume to the week
+  acc[weekNum] += workout.totalVolume;
+  return acc;
+  }, {});
   }
     
+
+
+
+  const checkIfWeekInProgress = (weekNumber: number | null) => {
+
+    const weekToDelete = weeks.find((w) => w.weekNumber === weekNumber);
+    const someCompletedWorkouts = weekToDelete?.days.some(d => d.isCompleted);
+
+    if (someCompletedWorkouts) {
+      setWarningMessage(true);
+      return;
+    }
+
+    setWeekToDelete(weekNumber)
+    setDeleteWeekPopUp(true);
+  }
+
+ 
+
+  const handleDeleteWeek = async (weekToDelete: number | null) => {
+    if (weekToDelete === null || !workoutPlanId) return;
+  
+    try {
+      await deleteWeekApi({ workoutPlanId, weekNumber: weekToDelete });
+      dispatch(deleteWeek(weekToDelete));
+    } catch (error) {
+      console.error('Could not delete week:', error);
+    } finally {
+      setDeleteWeekPopUp(false);
+      setWeekToDelete(null);
+    }
+  };
+  
+
   
 
 
-    // work on adding functionality to the delete marker on each card
-    const handleDeleteWeek = async (weekNumber: number) => {
-       const weekToDelete = weeks.find((w) => w.weekNumber === weekNumber);
-
-       const someCompletedWorkouts = weekToDelete?.days.some(d => d.isCompleted);
-
-       if(someCompletedWorkouts) {
-        window.alert('you cannot delete a week with completed workouts');
-        return;
-       }
-
-      if(window.confirm('are you sure you want to delete this week?')) {
-      
-        try {
-          
-        await deleteWeekApi({
-            workoutPlanId: workoutPlanId,
-            weekNumber: weekNumber
-          })
-
-          dispatch(deleteWeek(weekNumber));
-
-        } catch (error) {
-          console.error('could not complete request to delete week', error);
-        }
-      
-      }
-
-    }
     
     return (
       <>
         {weeks.map((week, index) => {
           const weekVolume = volumeByWeek[week.weekNumber] || 0;
           const isWeekCompleted = week.days.every(day => day.isCompleted);
+    
           return (
             <div className="week-card-div" key={index}>
                 {isEditing && (
                     <div className="week-card-delete-marker-div">
                         <img src={deleteMarker} alt="" className="week-card-delete-marker" onClick={() => {
-                            handleDeleteWeek(week.weekNumber);
+                         checkIfWeekInProgress(week.weekNumber);
                         }}/>
                     </div>
                 )}
@@ -170,9 +180,60 @@ const WeekCard: React.FC<WeekCardProps> = ({ isEditing }) => {
                   </p>
                 ))}
               </div>
+            
             </div>
+            
           );
         })}
+          {warningMessage && (
+              <div className="custom-modal-overlay">
+    <div className="custom-modal-content">
+      <h2 className="custom-modal-title">Action Not Allowed</h2>
+      <p className="custom-modal-message">
+        You cannot delete a week with completed workouts.
+      </p>
+      <div className="custom-modal-buttons">
+        <button
+          className="cancel-btn"
+          onClick={() => { 
+            setWarningMessage(false);
+            setWeekToDelete(null);
+         }}
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  </div>
+            )}
+            {deleteWeekPopUp && (
+               <div className="custom-modal-overlay">
+               <div className="custom-modal-content">
+                 <h2 className="custom-modal-title">Delete Week</h2>
+                 <p className="custom-modal-message">
+                   Are you sure you want to delete this week from your plan?
+                 </p>
+                 <div className="custom-modal-buttons">
+                   <button
+                     className="confirm-btn"
+                     onClick={() => handleDeleteWeek(weekToDelete)}
+                   >
+                     Yes, delete
+                   </button>
+                   <button
+                     className="cancel-btn"
+                     onClick={() => {
+                      setDeleteWeekPopUp(false);
+                      setWeekToDelete(null);
+                     }
+                     }
+                   >
+                     Cancel
+                   </button>
+                 </div>
+               </div>
+             </div>
+            )}
       </>
     );
   };
