756876a1f7facb40989cc269ab57a195a9906271
Brendan Buchanan <brendanbuchanan21@gmail.com>
2025-04-05 09:26:21 -0500

    added pop up to todaysworkoutpage for confirm deletion of exercise

--- frontend/src/components/workouts/currentPlan/todaysWorkoutPage.tsx
+++ frontend/src/components/workouts/currentPlan/todaysWorkoutPage.tsx
@@ -19,279 +19,266 @@ import backArrow from '../../../images/backArrow.svg';
 
 const TodaysWorkoutPage = () => {
 
-    const dispatch = useDispatch();
-    const currentPlan = useSelector((state: RootState) => state.workout.currentPlan);
+  const dispatch = useDispatch();
+  const currentPlan = useSelector((state: RootState) => state.workout.currentPlan);
 
-
-
+  const firstIncompleteWeek = currentPlan?.weeks.find((week) =>
+  week.days.some((day) => !day.isCompleted)
+  );
+  const firstIncompleteWorkout = firstIncompleteWeek?.days.find((day) => !day.isCompleted);
   
-
-    const firstIncompleteWeek = currentPlan?.weeks.find((week) =>
-      week.days.some((day) => !day.isCompleted)
-    );
-    const firstIncompleteWorkout = firstIncompleteWeek?.days.find((day) => !day.isCompleted);
-
-
-  
-    if(!firstIncompleteWorkout) {
-      return;
-    }
+  if(!firstIncompleteWorkout) {
+  return;
+  }
     
-    const [editMode, setEditMode] = useState(false);
+  // usestate section
+  const [editMode, setEditMode] = useState(false);
+  const [showDeletePopUp, setShowDeletePopUp] = useState(false);
+  const [exerciseToDelete, setExerciseToDelete] = useState<Exercise | null>(null);
+  const [cantDeletePopUp, setCantDeletePopUp] = useState(false);
 
-   const navigate = useNavigate();
+  const navigate = useNavigate();
     
 
-    const [completedExercises, setCompletedExercises] = useState<{ [key: string]: boolean }>({});
-    const [postCompletedExercise] = usePostCompletedExerciseMutation(); 
-    const [updateCompletedWorkout] = useUpdateWorkoutCompletionApiMutation();
+  const [completedExercises, setCompletedExercises] = useState<{ [key: string]: boolean }>({});
+  const [postCompletedExercise] = usePostCompletedExerciseMutation(); 
+  const [updateCompletedWorkout] = useUpdateWorkoutCompletionApiMutation();
 
-    const toggleExerciseCompletion = (exerciseId: string) => {
-      setCompletedExercises((prev) => ({
-        ...prev,
-        [exerciseId]: !prev[exerciseId], // Toggle completion state
-      }));
-    };
+
+
+const toggleExerciseCompletion = (exerciseId: string) => {
+  setCompletedExercises((prev) => ({
+  ...prev,
+  [exerciseId]: !prev[exerciseId], // Toggle completion state
+  }));
+};
 
  
 
-    const handleSetChange = (exerciseId: string | any, setId: string | any, field: keyof SetDetails, value: string | number) => {
+const handleSetChange = (exerciseId: string | any, setId: string | any, field: keyof SetDetails, value: string | number) => {
       
       
-      const parsedValue = value === "" ? null : Number(value);
+  const parsedValue = value === "" ? null : Number(value);
 
-      dispatch(
-        updateSetDetails({
-          weekNumber,
-          day: firstIncompleteWorkout.day,
-          exerciseId,
-          setId,
-          updatedSet: { [field]: parsedValue },
-        })
-      );
-    };
+  dispatch(
+  updateSetDetails({
+  weekNumber,
+  day: firstIncompleteWorkout.day,
+  exerciseId,
+  setId,
+  updatedSet: { [field]: parsedValue },
+  })
+  );
+};
 
   
-    if (!currentPlan || !currentPlan.weeks) {
-        return <h1>No active workout plan found.</h1>;
-    }
+  if (!currentPlan || !currentPlan.weeks) {
+  return <h1>No active workout plan found.</h1>;
+  }
 
 
-    const [addingSetToExercise] = useAddSetToExerciseApiMutation();
-    const [deleteSetFromExercise] = useDeleteSetFromExerciseApiMutation();
-    const [deleteExerciseApi] = useDeleteExerciseApiMutation();
+  const [addingSetToExercise] = useAddSetToExerciseApiMutation();
+  const [deleteSetFromExercise] = useDeleteSetFromExerciseApiMutation();
+  const [deleteExerciseApi] = useDeleteExerciseApiMutation();
     
 
-   const weekNumber = firstIncompleteWeek?.weekNumber;
+  const weekNumber = firstIncompleteWeek?.weekNumber;
   
-    const handleAddSet = async (exercise: Exercise) => {
+const handleAddSet = async (exercise: Exercise) => {
 
-      try {
+  try {
 
-        if (currentPlan && currentPlan._id && weekNumber !== null && firstIncompleteWorkout && exercise._id) {
-          const newSetNumber = exercise.sets.length + 1;
-          const newSet: SetDetails = {
-            setNumber: newSetNumber,
-            reps: null,
-            weight: null,
-            rir: null,
-          };
+  if (currentPlan && currentPlan._id && weekNumber !== null && firstIncompleteWorkout && exercise._id) {
+  const newSetNumber = exercise.sets.length + 1;
+  const newSet: SetDetails = {
+  setNumber: newSetNumber,
+  reps: null,
+  weight: null,
+  rir: null,
+  };
          
-          const result = await addingSetToExercise({
-            workoutId: currentPlan._id,
-            weekNumber,
-            day: firstIncompleteWorkout.day,
-            exerciseId: exercise._id,
-            newSet,
-          });
-          if (result.data && result.data.newSet._id) { // Check if the result and ID exist
-            const newSetWithId = {
-              ...newSet,
-              _id: result.data.newSet._id, // Add the returned ID
-            };
+  const result = await addingSetToExercise({
+  workoutId: currentPlan._id,
+  weekNumber,
+  day: firstIncompleteWorkout.day,
+  exerciseId: exercise._id,
+  newSet,
+  });
+  if (result.data && result.data.newSet._id) { // Check if the result and ID exist
+  const newSetWithId = {
+  ...newSet,
+  _id: result.data.newSet._id, // Add the returned ID
+  };
     
     
-          dispatch(
-            addSetToExercise({
-              weekNumber,
-              day: firstIncompleteWorkout.day,
-              exerciseId: exercise._id,
-              newSet: newSetWithId,
-            })
-          );
-        } else {
-          console.error('Failed to get the new set ID from the backend.');
-        }
-      } else {
-        console.warn('Missing required data to add set.');
-      }
-    } catch (error) {
-      console.error('Error adding set:', error);
-    }
+  dispatch(
+  addSetToExercise({
+  weekNumber,
+  day: firstIncompleteWorkout.day,
+  exerciseId: exercise._id,
+  newSet: newSetWithId,
+  })
+  );
+  } else {
+  console.error('Failed to get the new set ID from the backend.');
+  }
+  } else {
+  console.warn('Missing required data to add set.');
+  }
+  } catch (error) {
+  console.error('Error adding set:', error);
+  }
   };
 
 
-  const deleteSet = async (set: SetDetails, exercise: Exercise) => {
-      try { 
-        if (currentPlan && currentPlan._id && weekNumber !== null && firstIncompleteWorkout && exercise._id && set._id) {
-            await deleteSetFromExercise({
-              workoutId: currentPlan._id,
-              weekNumber,
-              day: firstIncompleteWorkout.day,
-              exerciseId: exercise._id,
-              setId: set._id,
-            });
-            dispatch(
-              removeSetFromExercise({
-                weekNumber,
-                day: firstIncompleteWorkout.day,
-                exerciseId: exercise._id,
-                setId: set._id,
-              })
-            );
-          } else {
-            console.warn('Missing required data to delete set.');
-          }
-        } catch (error) {
-          console.error('Error deleting set:', error);
-        }
+const deleteSet = async (set: SetDetails, exercise: Exercise) => {
+  try { 
+  if (currentPlan && currentPlan._id && weekNumber !== null && firstIncompleteWorkout && exercise._id && set._id) {
+  await deleteSetFromExercise({
+  workoutId: currentPlan._id,
+  weekNumber,
+  day: firstIncompleteWorkout.day,
+  exerciseId: exercise._id,
+  setId: set._id,
+  });
+
+  dispatch(
+  removeSetFromExercise({
+  weekNumber,
+  day: firstIncompleteWorkout.day,
+  exerciseId: exercise._id,
+  setId: set._id,
+  }));
+
+  } else {
+  console.warn('Missing required data to delete set.');
   }
+  } catch (error) {
+  console.error('Error deleting set:', error);
+  }
+}
 
-  const deleteExerciseFromCurrentDay = async (exercise: Exercise) => {
+const deleteExerciseFromCurrentDay = async (exercise: Exercise) => {
 
-    if (!window.confirm("Are you sure you want to delete this exercise?")) {
-      return; // Exit if the user cancels.
-    }
+  try {
+  if(exercise._id && currentPlan &&
+  currentPlan._id &&
+  weekNumber !== null &&
+  firstIncompleteWorkout) {
 
-    try {
-      if(exercise._id && currentPlan &&
-        currentPlan._id &&
-        weekNumber !== null &&
-        firstIncompleteWorkout) {
-
-        await deleteExerciseApi({
-          workoutId: currentPlan._id,
-          exerciseId: exercise._id,
-          weekNumber,
-          day: firstIncompleteWorkout.day
-        }).unwrap();
+  await deleteExerciseApi({
+  workoutId: currentPlan._id,
+  exerciseId: exercise._id,
+  weekNumber,
+  day: firstIncompleteWorkout.day
+  }).unwrap();
 
 
-        dispatch(deleteExercise({
-          weekNumber,
-          day: firstIncompleteWorkout.day,
-          exerciseId: exercise._id
-        }))
+  dispatch(deleteExercise({
+  weekNumber,
+  day: firstIncompleteWorkout.day,
+  exerciseId: exercise._id
+  }))
 
-      } else {
-        console.warn('missing data required for exercise deletion');
-      }
+  } else {
+  console.warn('missing data required for exercise deletion');
+  }
 
      
-    } catch (error) {
-      console.error('the try block did not execute:', error);
-    }
-
+  } catch (error) {
+  console.error('the try block did not execute:', error);
   }
 
+}
 
-  // handle submitting the workout 
 
 
-  const sendCompletedExercise = async (completedWorkout: any) => {
-    try {
-    
-      const response = await postCompletedExercise(completedWorkout).unwrap();
 
-    } catch (error) {
-      console.error("API request failed:", JSON.stringify(error, null, 2));
-    }
+
+const sendCompletedExercise = async (completedWorkout: any) => {
+  try {
+  const response = await postCompletedExercise(completedWorkout).unwrap();
+  } catch (error) {
+  console.error("API request failed:", JSON.stringify(error, null, 2));
   }
 
-  const completedWorkoutUpdate = async (completedWorkout: any) => {
-    try {
-      const response = await updateCompletedWorkout({
-        workoutPlanId: completedWorkout.workoutPlanId,
-        weekNumber: completedWorkout.weekNumber,
-        day: completedWorkout.day,
-        isCompleted: true 
-      }).unwrap();
+}
+
+const completedWorkoutUpdate = async (completedWorkout: any) => {
+  try {
+  const response = await updateCompletedWorkout({
+  workoutPlanId: completedWorkout.workoutPlanId,
+  weekNumber: completedWorkout.weekNumber,
+  day: completedWorkout.day,
+  isCompleted: true 
+  }).unwrap();
    
 
-      dispatch(updateDayCompletion({
-        weekNumber: completedWorkout.weekNumber,
-        day: completedWorkout.day,
-        isCompleted: true
-      }))
-    } catch (error) {
-      console.error(error, 'we could not update the workout to complete');
-      return;
-    }
+  dispatch(updateDayCompletion({
+  weekNumber: completedWorkout.weekNumber,
+  day: completedWorkout.day,
+  isCompleted: true
+  }))
+
+  } catch (error) {
+  console.error(error, 'we could not update the workout to complete');
+  return;
   }
+}
 
 
 
 
 
-  const handleSubmitWorkout = async () => {
+const handleSubmitWorkout = async () => {
 
-        const allComplete = firstIncompleteWorkout.exercises.every((exercise) => {
-          return exercise._id ? completedExercises[exercise._id] : false;
-        });
-        if (!allComplete) {
-          alert("Please mark all exercises as complete before submitting the workout.");
-          return;
-        }
+  const allComplete = firstIncompleteWorkout.exercises.every((exercise) => {
+  return exercise._id ? completedExercises[exercise._id] : false;
+  });
+  if (!allComplete) {
+  alert("Please mark all exercises as complete before submitting the workout.");
+  return;
+  }
         
-        // this is to extract the week number for the completedworkout below
-        if(firstIncompleteWorkout) {
-          let weekNumber = null;
-            for (const week of currentPlan.weeks) {
-                const foundDay = week.days.find(day => day._id === firstIncompleteWorkout._id);
-                if (foundDay) {
-                    weekNumber = week.weekNumber;
-                    break; // Stop iterating once found
-                }
-        }
-      } else {
-        console.error('Workout day _id is missing.');
-        return;
-      }
-
-        //directly pull data from the currentplan to send to backend
-        if(currentPlan && currentPlan.weeks && currentPlan.weeks[0] && currentPlan.weeks[0].days[0]) {
-
-          const completedWorkout = {
-            workoutPlanId: currentPlan._id,
-            weekNumber: weekNumber,
-            day: firstIncompleteWorkout.day,
-            exercises: firstIncompleteWorkout.exercises
-
-          }
-          try {
-            sendCompletedExercise(completedWorkout);
-            completedWorkoutUpdate(completedWorkout)
-
-           
-          
-
-          } catch (error) {
-            console.error("❌ Error submitting workout, staying on page:", error);
-          }
-          
-         
-            navigate('/workouts');
-          
-          
-          
-        } else {
-          console.error('workout plan data is missing');
-        }
+  // this is to extract the week number for the completedworkout below
+  if(firstIncompleteWorkout) {
+  let weekNumber = null;
+  for (const week of currentPlan.weeks) {
+  const foundDay = week.days.find(day => day._id === firstIncompleteWorkout._id);
+  if (foundDay) {
+  weekNumber = week.weekNumber;
+  break; // Stop iterating once found
+  }
+  }
+  } else {
+  console.error('Workout day _id is missing.');
+  return;
   }
 
-  const handleBackClick = () => {
+  //directly pull data from the currentplan to send to backend
+  if(currentPlan && currentPlan.weeks && currentPlan.weeks[0] && currentPlan.weeks[0].days[0]) {
+
+  const completedWorkout = {
+  workoutPlanId: currentPlan._id,
+  weekNumber: weekNumber,
+  day: firstIncompleteWorkout.day,
+  exercises: firstIncompleteWorkout.exercises
+  }
+  try {
+  sendCompletedExercise(completedWorkout);
+  completedWorkoutUpdate(completedWorkout)
+  } catch (error) {
+  console.error("❌ Error submitting workout, staying on page:", error);
+  }
+  navigate('/workouts');
+  } else {
+  console.error('workout plan data is missing');
+  }
+}
+
+const handleBackClick = () => {
     return navigate('/workouts');
-  }
+}
   
 
 
@@ -317,7 +304,15 @@ const TodaysWorkoutPage = () => {
       <div key={exercise._id} className="exercise-card">
         <div className="exercise-card-header-div">
           <h3>{exercise.name}</h3>
-          <img src={trashImg} alt="" className="trash-img" onClick={() => deleteExerciseFromCurrentDay(exercise)} />
+          <img src={trashImg} alt="" className="trash-img" onClick={() => {
+           if (firstIncompleteWorkout.exercises.length >= 2) {
+            setExerciseToDelete(exercise);
+            setShowDeletePopUp(true);
+           } else {
+            setCantDeletePopUp(true);
+           }
+           
+          }}/>
         </div>
 
         <div className="exercise-card-button-container">
@@ -408,6 +403,57 @@ const TodaysWorkoutPage = () => {
   <div className="submit-todays-workout-div">
       <button onClick={handleSubmitWorkout} className="complete-workout-btn">Complete Workout</button>
   </div>
+  {showDeletePopUp && exerciseToDelete && (
+  <div className="custom-modal-overlay">
+    <div className="custom-modal-content">
+      <h2 className="custom-modal-title">Delete Exercise</h2>
+      <p className="custom-modal-message">
+        Are you sure you want to delete <strong>{exerciseToDelete.name}</strong>?
+      </p>
+      <div className="custom-modal-buttons">
+        <button
+          className="confirm-btn"
+          onClick={() => {
+            deleteExerciseFromCurrentDay(exerciseToDelete);
+            setShowDeletePopUp(false);
+            setExerciseToDelete(null);
+          }}
+        >
+          Yes, delete
+        </button>
+        <button
+          className="cancel-btn"
+          onClick={() => {
+            setShowDeletePopUp(false);
+            setExerciseToDelete(null);
+          }}
+        >
+          Cancel
+        </button>
+      </div>
+    </div>
+  </div>
+)}
+{cantDeletePopUp && (
+   <div className="custom-modal-overlay">
+   <div className="custom-modal-content">
+     <h2 className="custom-modal-title">Can't Delete Exercise</h2>
+     <p className="custom-modal-message">
+       You can't delete the only exercise in your workout. Add another exercise before removing this one.
+     </p>
+     <div className="custom-modal-buttons">
+       <button
+         className="cancel-btn"
+         onClick={() => setCantDeletePopUp(false)}
+       >
+         Close
+       </button>
+     </div>
+   </div>
+ </div>
+)}
+
+
 </section>
         </>
     )
